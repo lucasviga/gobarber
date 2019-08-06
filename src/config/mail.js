b03862000215185990083400fa4ca685059a597c
Lucas Viga <lucasviga12@gmail.com>
2019-08-05 22:05:09 -0400

    configure mail + mailtrap.io

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '314fcbd99251e9',
+    pass: 'aec7fad0804f99',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
