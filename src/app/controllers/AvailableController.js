80db5f756cf96c861a2ca70361f6cffda8604f17
Lucas Viga <lucasviga12@gmail.com>
2019-08-07 20:15:13 -0400

    create available controller

---
+++ src/app/controllers/AvailableController.js
@@ -0,0 +1,31 @@
+import { startOfDay, endOfDay } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+
+class AvailableController {
+  async index(req, res) {
+    const { date } = req.query;
+
+    // console.log(req.query.date);
+
+    if (!date) {
+      return res.status(400).json({ error: 'Invalide date' });
+    }
+
+    const searchDate = Number(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new AvailableController();
--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,6 +25,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
